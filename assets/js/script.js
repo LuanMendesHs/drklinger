62a9c73a9f6e50867df7b5e7ec7cb6a09396d6c9
unknown <familia.martinez.2009@hotmail.com>
2024-12-26 15:40:12 -0300

    Section dicas completo, Section perguntas iniciado, porem ainda não concluido no JS e organização do JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,51 @@
-const header = document.getElementById("header-js");
-const carrosel = document.querySelector(".carrosel");
-
-function menuTopo() {
-    const alturaTopo = carrosel.getBoundingClientRect().top;
-    if(alturaTopo < 60) {
-        header.classList.add("ativo")
-    } else {
-        header.classList.remove("ativo")
+// inicio efeito header topo
+function initEfeitoHeader(){
+    const header = document.getElementById("header-js");
+    const carrosel = document.querySelector(".carrosel");
+    
+    function menuTopo() {
+        const alturaTopo = carrosel.getBoundingClientRect().top;
+        if(alturaTopo < 60) {
+            header.classList.add('ativo');
+        } else {
+            header.classList.remove('ativo');
+        }
     }
+    
+    window.addEventListener('scroll', menuTopo);
 }
 
-window.addEventListener('scroll', menuTopo)
+initEfeitoHeader();
+// fim efeito header topo
 
 
 // inicio section sobre
-const sobre = document.querySelector('.sobre')
-const windowMetade = window.innerHeight * 0.6;
-
-function sobreScrollEfect() {
-    const sobreTopo = sobre.getBoundingClientRect().top - windowMetade;
-    if(sobreTopo < 0) {
-        sobre.classList.add('ativo');
-        console.log(sobre, sobreTopo)
+function initEfeitoScrollSobre(){
+    const sobre = document.querySelector('.sobre');
+    const windowMetade = window.innerHeight * 0.6;
+    
+    function sobreScrollEfect() {
+        const sobreTopo = sobre.getBoundingClientRect().top - windowMetade;
+        if(sobreTopo < 0) {
+            sobre.classList.add('ativo');
+        }
     }
+    window.addEventListener('scroll', sobreScrollEfect);
 }
-window.addEventListener('scroll', sobreScrollEfect)
+initEfeitoScrollSobre();
+// fim section sobre
+
+// inicio accordion perguntas frequentes
+const accordionList = document.querySelectorAll('.js-perguntas h4');
+
+function activeAccordion() {
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+})
+
+
+// fim accordion perguntas frequentes
